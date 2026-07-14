d44ef1f3d93f1d7fffed82a45be18c6ed4f51802
Ed Wright <edwinwright@gmail.com>
2026-07-14 11:17:05 +0200

    Add and configure the post mainImage

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -26,6 +26,7 @@ export const postType = defineType({
 		defineField({
 			name: "mainImage",
 			type: "image",
+			description: "The main image of the post",
 			options: {
 				hotspot: true,
 			},
@@ -34,6 +35,13 @@ export const postType = defineType({
 					name: "alt",
 					type: "string",
 					title: "Alternative text",
+					validation: (rule) =>
+						rule.custom((value, context) => {
+							const parent = context?.parent as { asset?: { _ref?: string } };
+							return !value && parent?.asset?._ref
+								? "Alternative text is required when an image is present"
+								: true;
+						}),
 				}),
 			],
 		}),
